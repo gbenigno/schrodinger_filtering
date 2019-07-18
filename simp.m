36b9ac73465f190ebff6af4d06aefbd159546e90
gbenigno <44267555+gbenigno@users.noreply.github.com>
2019-07-18 14:50:05 -0400

    Add files via upload

---
+++ simp.m
@@ -0,0 +1,23 @@
+    %**********************************************************************
+    %*********              Numerical integration                 *********
+    %**********************************************************************
+
+    % Author: Taous Meriem Laleg
+
+    function y = simp(f,dx)
+    %  This function returns the numerical integration of a function f
+    %  using the Simpson method
+
+    n=length(f);
+    I(1)=1/3*f(1)*dx;
+    I(2)=1/3*(f(1)+f(2))*dx;
+
+    for i=3:n
+        if(mod(i,2)==0)
+            I(i)=I(i-1)+(1/3*f(i)+1/3*f(i-1))*dx;
+        else
+            I(i)=I(i-1)+(1/3*f(i)+f(i-1))*dx;
+        end
+    end
+    y=I(n);
+    end
