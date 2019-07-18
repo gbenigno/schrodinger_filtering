8b798b142ae2062523f124355128262e45bc1bab
gbenigno <44267555+gbenigno@users.noreply.github.com>
2019-07-18 15:13:56 -0400

    Add files via upload

---
+++ sf_algo.m
@@ -0,0 +1,38 @@
+function [EEG_aas_spikeless,EEG_sf,globfilt] = sf_algo(ch_vec,EEG_aas,EEG_input_for_gf,EEG_raw,globfilt,seq,trigs)
+
+% author: Gabriel Benigno
+
+% performs Schrodinger filtering algorithm
+
+%%% inputs:
+% ch_vec: vector of channels to loop through
+% EEG_aas: dataset following AAS
+% EEG_input_for_gf: the dataset to be inputted for global filtering
+% EEG_raw: raw dataset
+% globfilt: structure array contain various data from global filtering step
+% seq: sequence of steps. ie [1 1] for both ds and gf; [1 0] for ds but not gf; and [0 1] for gf but not ds
+% trigs: vector of slice acquisition trigger timings
+
+    if seq(1)==1 % if despiking is selected
+
+        %%%%%%%% STEP 1: DE-SPIKING %%%%%%%
+        EEG_aas_spikeless = sf_ds(ch_vec,EEG_aas,EEG_raw,trigs);
+
+        if seq(2)==1
+            %% %%%%%% STEP 2: GLOBAL FILTERING (FOLLOWING DE-SPIKING) %%%%%%%
+            EEG_input_for_gf = EEG_aas_spikeless;
+            [EEG_sf,globfilt] = sf_gf(ch_vec,EEG_aas,EEG_input_for_gf,EEG_raw,globfilt,trigs);
+        end
+
+        % if seq(2)=0 within seq(1)=1, nothing is done
+
+    elseif seq(1)==0 && seq(2)==1
+
+        %%%%%%%%% ALTERNATE: GLOBAL FILTERING (WITHOUT DE-SPIKING) %%%%%%%
+        [EEG_sf,globfilt] = sf_gf(ch_vec, EEG_aas, EEG_input_for_gf, EEG_raw, globfilt, trigs);
+        EEG_aas_spikeless=[];
+
+    end
+
+end
+
